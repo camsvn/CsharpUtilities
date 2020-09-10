db80c7ea81c399e063159c2cf53649466e34b6aa
Amal Salvin Joseph <39452708+camsvn@users.noreply.github.com>
2020-09-11 01:07:31 +0530

    Http using WebRequests in vs2008 C#

---
+++ Http4vs2008.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace HTTPLEARN
+{
+    class Program
+    {
+        public void get()
+        {
+            WebRequest request = WebRequest.Create("http://www.contoso.com/PostAccepter.aspx ");
+            // Set the Method property of the request to POST.
+            request.Method = "POST";
+            // Create POST data and convert it to a byte array.
+            string postData = "This is a test that posts this string to a Web server.";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.
+            dataStream.Close();
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            string sURL;
+            sURL = "http://DESKTOP-8560w:8080/api/dbupdate";
+
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+            //WebProxy myProxy = new WebProxy("myproxy", 80);
+            //myProxy.BypassProxyOnLocal = true;
+            //wrGETURL.Proxy = WebProxy.GetDefaultProxy();
+
+            Stream objStream;
+            try
+            {
+                objStream = wrGETURL.GetResponse().GetResponseStream();
+                StreamReader objReader = new StreamReader(objStream);
+
+                string sLine = "";
+                int i = 0;
+
+                while (sLine != null)
+                {
+                    i++;
+                    sLine = objReader.ReadLine();
+                    if (sLine != null)
+                        Console.WriteLine("{0}", sLine);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }            
+            Console.ReadLine();
+        }
+    }
+}
